1dddf682483b47e34f4385b773d7674e081f8aad
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2023-12-16 10:20:37 +0800

    Update rotate_direction.rs

--- sfinder-lib/src/sfinder_core/srs/rotate_direction.rs
+++ sfinder-lib/src/sfinder_core/srs/rotate_direction.rs
@@ -18,11 +18,19 @@ const VALUES_WITH_180: [RotateDirection; 3] = [
 ];
 
 impl RotateDirection {
-    fn values_no_180() -> &'static [RotateDirection] {
+    pub fn reverse(self) -> Self {
+        match self {
+            RotateDirection::Clockwise => RotateDirection::CounterClockwise,
+            RotateDirection::CounterClockwise => RotateDirection::Clockwise,
+            RotateDirection::Rotate180 => RotateDirection::Rotate180,
+        }
+    }
+
+    pub fn values_no_180() -> &'static [RotateDirection] {
         &VALUES_NO_180
     }
 
-    fn values_with_180() -> &'static [RotateDirection] {
+    pub fn values_with_180() -> &'static [RotateDirection] {
         &VALUES_WITH_180
     }
 }
