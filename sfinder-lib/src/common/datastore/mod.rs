582597371c7c8406f0b758bd66c1a500ee0dacc5
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2023-12-27 13:25:35 +0800

    implement OperationComparator into Operation + tests

--- sfinder-lib/src/common/datastore/mod.rs
+++ sfinder-lib/src/common/datastore/mod.rs
@@ -6,3 +6,4 @@ pub mod mino_operation;
 pub mod mino_operation_with_key;
 pub mod operation;
 pub mod operation_with_key;
+pub mod simple_operation;
--- sfinder-lib/src/common/datastore/operation.rs
+++ sfinder-lib/src/common/datastore/operation.rs
@@ -24,3 +24,106 @@ where
             + u64::from(self.get_x())
     }
 }
+
+impl<Coord> PartialEq for dyn Operation<Coord>
+where
+    u32: From<Coord>,
+    u64: From<Coord>,
+    Coord: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.get_piece() == other.get_piece()
+            && self.get_rotate() == other.get_rotate()
+            && self.get_x() == other.get_x()
+            && self.get_y() == other.get_y()
+    }
+}
+
+impl<Coord> PartialOrd for dyn Operation<Coord>
+where
+    u32: From<Coord>,
+    u64: From<Coord>,
+    Coord: Ord,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.get_piece().cmp(&other.get_piece()).then_with(|| {
+            self.get_rotate().cmp(&other.get_rotate()).then_with(|| {
+                self.get_x()
+                    .cmp(&other.get_x())
+                    .then_with(|| self.get_y().cmp(&other.get_y()))
+            })
+        }))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        common::datastore::simple_operation::SimpleOperation, sfinder_core::srs::rotate::Rotate,
+    };
+
+    fn test_operations(
+        operation1: &dyn Operation<u8>,
+        operation2: &dyn Operation<u8>,
+    ) -> Option<std::cmp::Ordering> {
+        operation1.partial_cmp(operation2)
+    }
+
+    fn test_symmetry(operation1: &dyn Operation<u8>, operation2: &dyn Operation<u8>) {
+        match test_operations(operation1, operation2) {
+            Some(std::cmp::Ordering::Greater) => assert_eq!(
+                test_operations(operation2, operation1),
+                Some(std::cmp::Ordering::Less)
+            ),
+            Some(std::cmp::Ordering::Less) => assert_eq!(
+                test_operations(operation2, operation1),
+                Some(std::cmp::Ordering::Greater)
+            ),
+            _ => panic!("invalid test input"),
+        }
+    }
+
+    #[test]
+    fn compare() {
+        assert_eq!(
+            test_operations(
+                &SimpleOperation::new(Piece::I, Rotate::Spawn, 0, 1),
+                &SimpleOperation::new(Piece::I, Rotate::Spawn, 0, 1)
+            ),
+            Some(std::cmp::Ordering::Equal)
+        );
+    }
+
+    #[test]
+    fn compare_diff_block() {
+        test_symmetry(
+            &SimpleOperation::new(Piece::S, Rotate::Spawn, 0, 1),
+            &SimpleOperation::new(Piece::J, Rotate::Spawn, 7, 1),
+        );
+    }
+
+    #[test]
+    fn compare_diff_rotate() {
+        test_symmetry(
+            &SimpleOperation::new(Piece::S, Rotate::Left, 0, 1),
+            &SimpleOperation::new(Piece::J, Rotate::Right, 7, 1),
+        );
+    }
+
+    #[test]
+    fn compare_diff_x() {
+        test_symmetry(
+            &SimpleOperation::new(Piece::I, Rotate::Spawn, 0, 1),
+            &SimpleOperation::new(Piece::I, Rotate::Spawn, 7, 1),
+        );
+    }
+
+    #[test]
+    fn compare_diff_y() {
+        test_symmetry(
+            &SimpleOperation::new(Piece::I, Rotate::Spawn, 0, 1),
+            &SimpleOperation::new(Piece::I, Rotate::Spawn, 0, 4),
+        );
+    }
+}
