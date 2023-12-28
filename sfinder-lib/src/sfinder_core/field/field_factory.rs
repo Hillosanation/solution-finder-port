d026bbf859f2f29734c289a137bf1fd95312328c
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2023-12-28 20:51:05 +0800

    implement MiddleField to  FieldFactory

--- sfinder-lib/src/sfinder_core/field/field_factory.rs
+++ sfinder-lib/src/sfinder_core/field/field_factory.rs
@@ -1,11 +1,11 @@
-use super::{field::Field, small_field::SmallField};
+use super::{field::Field, middle_field::MiddleField, small_field::SmallField};
 
 // TODO: replace 6 with MAX_FIELD_HEIGHT
 pub fn create_field(max_height: u8) -> Box<dyn Field> {
     match max_height {
         ..=6 => Box::new(SmallField::new()),
-        7..=12 => todo!(),
-        13..=24 => todo!(),
+        7..=12 => Box::new(MiddleField::new()),
+        13..=24 => todo!("LargeField"),
         _ => panic!("Field height should be equal or less than 24: height={max_height}"),
     }
 }
@@ -33,17 +33,14 @@ pub fn create_small_field_with_marks(marks: String) -> SmallField {
     create_small_field_with_marks_and_block(marks, true)
 }
 
-fn create_small_field_with_marks_and_block(marks: String, is_block: bool) -> SmallField {
-    // these conditions are already checked
-    debug_assert!(marks.len() <= 60, "marks is too long for SmallField");
-    debug_assert_eq!(marks.len() % 10, 0, "marks length should be multiple of 10");
-
+/// This function does not guarentee that the marks can be set to the field and needs to be checked.
+fn set_marks_to_field(marks: String, field: &mut dyn Field, is_block: bool) {
+    assert!(marks.is_ascii());
     let max_y = (marks.len() / 10) as u8;
-    let mut field = create_small_field();
     let marks = marks.into_bytes();
+
     for y in 0..max_y {
         for x in 0..10 {
-            // dbg!(marks[((max_y - y - 1) * 10 + x) as usize], x, y);
             match marks[((max_y - y - 1) * 10 + x) as usize] {
                 b' ' | b'_' => {
                     if !is_block {
@@ -59,20 +56,44 @@ fn create_small_field_with_marks_and_block(marks: String, is_block: bool) -> Sma
             // dbg!(field.get_x_board());
         }
     }
+}
+
+pub fn create_small_field_with_marks_and_block(marks: String, is_block: bool) -> SmallField {
+    assert!(marks.len() <= 60, "marks is too long for SmallField");
+    assert_eq!(
+        marks.len() % 10,
+        0,
+        "length of marks should be multiple of 10"
+    );
+
+    let mut field = create_small_field();
+
+    set_marks_to_field(marks, &mut field, is_block);
 
     field
 }
 
-pub fn create_middle_field() {
-    todo!("MiddleField")
+pub fn create_middle_field() -> MiddleField {
+    MiddleField::new()
 }
 
-pub fn create_middle_field_with_marks(marks: String) -> SmallField {
+pub fn create_middle_field_with_marks(marks: String) -> MiddleField {
     create_middle_field_with_marks_and_block(marks, true)
 }
 
-fn create_middle_field_with_marks_and_block(marks: String, is_block: bool) -> SmallField {
-    todo!("MiddleField")
+pub fn create_middle_field_with_marks_and_block(marks: String, is_block: bool) -> MiddleField {
+    assert!(marks.len() <= 120, "marks is too long for MiddleField");
+    assert_eq!(
+        marks.len() % 10,
+        0,
+        "length of marks should be multiple of 10"
+    );
+
+    let mut field = create_middle_field();
+
+    set_marks_to_field(marks, &mut field, is_block);
+
+    field
 }
 
 pub fn create_large_field() {
@@ -84,8 +105,18 @@ fn create_large_field_with_marks(marks: String, is_block: bool) -> SmallField {
 }
 
 // TODO: niche use
-pub fn create_inverse_field(marks: String) -> ! {
-    todo!()
+pub fn create_inverse_field(marks: String) -> Box<dyn Field> {
+    assert_eq!(
+        marks.len() % 10,
+        0,
+        "length of marks should be multiple of 10"
+    );
+
+    match marks.len() / 10 {
+        ..=6 => Box::new(create_small_field_with_marks_and_block(marks, false)),
+        ..=12 => Box::new(create_middle_field_with_marks_and_block(marks, false)),
+        max_y => panic!("Field height is too large, {max_y}"),
+    }
 }
 
 pub fn from_colored_field() {
