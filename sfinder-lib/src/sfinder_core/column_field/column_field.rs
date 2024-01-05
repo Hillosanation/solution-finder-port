858b1ad85ee7b61eb7049de83ae844551dedf36a
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-05 20:59:54 +0800

    implement tests from both ColumnFieldComparatorTest ColumnSmallFieldTest

--- sfinder-lib/src/sfinder_core/column_field/column_field.rs
+++ sfinder-lib/src/sfinder_core/column_field/column_field.rs
@@ -1,4 +1,6 @@
-pub trait ColumnField {
+use std::fmt::Debug;
+
+pub trait ColumnField: Debug {
     // 指定した位置にブロックをおく
     fn set_block(&mut self, x: u8, y: u8, height: u8);
 
@@ -58,6 +60,11 @@ impl PartialOrd for dyn ColumnField + '_ {
 
 #[cfg(test)]
 mod tests {
+    use crate::{
+        extras::test_functions::assert_partialord_symmetric,
+        sfinder_core::{column_field::column_field_factory, field::field_constants::FIELD_WIDTH},
+    };
+
     use super::*;
     use rand::{thread_rng, Rng};
 
@@ -67,10 +74,33 @@ mod tests {
     fn cmp() {
         let mut rngs = thread_rng();
 
+        // Porting note: this test relies of the fact that set_block is able to overflow?
         for _ in 0..10000 {
             // same field
-            let height = rngs.gen_range(1..10);
-            let field1 = todo!("ColumnFieldFactory");
+            let height = rngs.gen_range(1..FIELD_WIDTH);
+            let mut field1 = column_field_factory::create_small_field();
+            let mut field2 = column_field_factory::create_small_field();
+
+            for _ in 0..rngs.gen_range(1..15) {
+                let x = rngs.gen_range(0..FIELD_WIDTH);
+                let y = rngs.gen_range(0..height);
+                field1.set_block(x, y, height);
+                field2.set_block(x, y, height);
+            }
+
+            assert_eq!(field1, field2);
+            assert_eq!(field2, field1);
+
+            // 1block different field
+            let x = rngs.gen_range(0..FIELD_WIDTH);
+            let y = rngs.gen_range(0..height);
+            if field1.is_empty(x, y, height) {
+                field1.set_block(x, y, height);
+            } else {
+                field1.remove_block(x, y, height);
+            }
+
+            assert_partialord_symmetric(field1, field2);
         }
     }
 }
--- sfinder-lib/src/sfinder_core/column_field/column_small_field.rs
+++ sfinder-lib/src/sfinder_core/column_field/column_small_field.rs
@@ -21,8 +21,9 @@ impl From<u64> for ColumnSmallField {
     }
 }
 
+// Porting note: follows the original Java behaviour, but you shouldn't need to rely on the wrapping behaviour in normal usage.
 const fn get_y_mask(x: u8, y: u8, height: u8) -> u64 {
-    1 << (y + height * x)
+    1u64.overflowing_shl((y + height * x) as u32).0
 }
 
 impl ColumnField for ColumnSmallField {
@@ -76,6 +77,12 @@ impl PartialEq for ColumnSmallField {
     }
 }
 
+impl PartialOrd for ColumnSmallField {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        <dyn ColumnField>::partial_cmp(self, other)
+    }
+}
+
 impl HashCode for ColumnSmallField {
     type Output = u64;
 
@@ -89,5 +96,323 @@ impl std::fmt::Debug for ColumnSmallField {
         f.debug_struct("ColumnSmallField")
             .field("board", &self.0)
             .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        extras::test_functions::assert_partialord_symmetric,
+        sfinder_core::column_field::column_field_factory,
+    };
+
+    use super::*;
+
+    #[test]
+    fn set_block() {
+        let mut field = ColumnSmallField::new();
+        let height = 4;
+
+        assert!(field.is_empty(0, 0, height));
+        field.set_block(0, 0, height);
+        assert!(!field.is_empty(0, 0, height));
+        field.remove_block(0, 0, height);
+        assert!(field.is_empty(0, 0, height));
+    }
+
+    #[test]
+    fn get_board() {
+        let mut field = ColumnSmallField::new();
+        let height = 4;
+
+        assert_eq!(field.get_board(0), 0);
+        field.set_block(0, 0, height);
+        assert_eq!(field.get_board(0), 1);
+    }
+
+    #[test]
+    fn get_board_count() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        assert_eq!(field.get_board_count(), 1);
+    }
+
+    #[test]
+    fn get_num_of_all_blocks() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        assert_eq!(field.get_num_of_all_blocks(), 4);
+    }
+
+    #[test]
+    fn merge() {
+        let height = 4;
+        #[rustfmt::skip]
+        let mut field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "___"
+            + "__X"
+            + "X__"
+            + "___",
+            height,
+        );
+        #[rustfmt::skip]
+        let expect = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "__X"
+            + "X__"
+            + "_X_",
+            height,
+        );
+
+        field1.merge(&field2);
+
+        assert_eq!(field1, expect);
+    }
+
+    #[test]
+    fn reduce() {
+        let height = 4;
+        #[rustfmt::skip]
+        let mut field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "_XX"
+            + "XX_"
+            + "_XX",
+            height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "_X_"
+            + "_X_"
+            + "_X_"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let expect = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "X__"
+            + "__X"
+            + "X__"
+            + "__X",
+            height,
+        );
+
+        field1.reduce(&field2);
+
+        assert_eq!(field1, expect);
+    }
+
+    #[test]
+    fn can_merge_1() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "___"
+            + "__X"
+            + "X__"
+            + "___",
+            height,
+        );
+
+        assert!(!field1.can_merge(&field2));
+    }
+
+    #[test]
+    fn can_merge_2() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "___"
+            + "__X"
+            + "__X"
+            + "__X",
+            height,
+        );
+
+        assert!(field1.can_merge(&field2));
+    }
+
+    #[test]
+    fn freeze() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        let mut freeze = field.prune();
+
+        freeze.set_block(0, 2, height);
+        freeze.set_block(0, 3, height);
+
+        #[rustfmt::skip]
+        let expect = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+
+        assert_ne!(freeze.as_ref() as &dyn ColumnField, &expect as &_);
+        assert_eq!(field, expect);
+    }
+
+    #[test]
+    fn equals() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let field3 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "__X"
+            + "__X"
+            + "XX_",
+            height,
+        );
+
+        assert_eq!(field1, field2);
+        assert_eq!(field2, field1);
+        assert_ne!(field1, field3);
+        assert_ne!(field3, field1);
+    }
+
+    #[test]
+    fn hash_code_1() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_",
+            height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_", height,
+        );
+        #[rustfmt::skip]
+        let field3 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "__X"
+            + "__X"
+            + "XX_", height,
+        );
+
+        assert_eq!(field1.hash_code(), field2.hash_code());
+        assert_ne!(field1.hash_code(), field3.hash_code());
+    }
+
+    #[test]
+    fn compare_to() {
+        let height = 4;
+        #[rustfmt::skip]
+        let field1 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_", height,
+        );
+        #[rustfmt::skip]
+        let field2 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "___"
+            + "X__"
+            + "_X_", height,
+        );
+        #[rustfmt::skip]
+        let field3 = column_field_factory::create_small_field_with_marks(
+            String::new()
+            + "XX_"
+            + "__X"
+            + "__X"
+            + "XX_", height,
+        );
+
+        assert_eq!(field1.partial_cmp(&field2), Some(std::cmp::Ordering::Equal));
+        assert_eq!(field2.partial_cmp(&field1), Some(std::cmp::Ordering::Equal));
+        assert_partialord_symmetric(field3, field1);
     }
 }
