d18cfacb37cf6cf6f21eac024e5f499e47c4b3a7
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-11 21:28:18 +0800

    implement MinoFields this should be the correct interface, after looking at ConnectionsToStreamCallable calculate since a new RecursiveMinoField is constructed, it must transfer ownership to the caller

---
+++ sfinder-lib/src/searcher/pack/mino_fields/mino_fields.rs
@@ -0,0 +1,7 @@
+use crate::searcher::pack::mino_field::mino_field::MinoField;
+
+pub trait MinoFields {
+    // TODO: Is boxed iterator necessary, compared to an generic associated type?
+    // TODO: Is boxed MinoField necessary, or is substituting it with RecursiveMinoField possible?
+    fn stream(&self) -> Box<dyn Iterator<Item = Box<dyn MinoField>>>;
+}
--- sfinder-lib/src/searcher/pack/mod.rs
+++ sfinder-lib/src/searcher/pack/mod.rs
@@ -1,5 +1,6 @@
 pub mod in_out_pair_field;
 pub mod mino_field;
+pub mod mino_fields;
 pub mod separable_mino;
 pub mod separable_minos;
 pub mod sized_bit;
