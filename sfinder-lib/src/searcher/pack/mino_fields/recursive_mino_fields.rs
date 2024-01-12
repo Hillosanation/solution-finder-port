793c8fee3cc1ac48d51a5e0b4516468afdd5fd09
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-12 10:54:16 +0800

    add impl MinoFields for RecursiveMinoFields it's just a cast from RecursiveMinoField to dyn MinoField

--- sfinder-lib/src/searcher/pack/mino_fields/recursive_mino_fields.rs
+++ sfinder-lib/src/searcher/pack/mino_fields/recursive_mino_fields.rs
@@ -1,7 +1,21 @@
 //! The items are guarenteed to be RecursiveMinoField, compared to MinoFields.
 
-use crate::searcher::pack::mino_field::recursive_mino_field::RecursiveMinoField;
+use super::mino_fields::MinoFields;
+use crate::searcher::pack::mino_field::{
+    mino_field::MinoField, recursive_mino_field::RecursiveMinoField,
+};
 
 pub trait RecursiveMinoFields {
     fn recursive_stream(&self) -> Box<dyn Iterator<Item = RecursiveMinoField> + '_>;
+}
+
+impl MinoFields for dyn RecursiveMinoFields + '_ {
+    // Porting note: refactored implementation here, since all implementors directly call recursiveStream anyways
+    // TODO: is this needed? Can I just use RecursiveMinoFields directly to avoid the boxing?
+    fn stream(&self) -> Box<dyn Iterator<Item = Box<dyn MinoField + '_>> + '_> {
+        Box::new(
+            self.recursive_stream()
+                .map(|mino_field| Box::new(mino_field) as _),
+        )
+    }
 }
