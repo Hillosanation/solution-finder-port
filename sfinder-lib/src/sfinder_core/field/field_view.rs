8ccfef5cea9961cf81d3a5384c249e5550d56916
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-13 01:06:12 +0800

    add FieldView tests

--- sfinder-lib/src/sfinder_core/field/field_view.rs
+++ sfinder-lib/src/sfinder_core/field/field_view.rs
@@ -48,3 +48,78 @@ pub fn to_reduced_string(field: &dyn Field) -> String {
         .collect::<Vec<String>>()
         .join("\n")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::sfinder_core::field::field_factory;
+    use rand::{thread_rng, Rng};
+
+    #[test]
+    fn test_random() {
+        let mut rngs = thread_rng();
+
+        for _ in 0..10000 {
+            let height = rngs.gen_range(1..=12);
+            let empty_spots: Vec<[_; FIELD_WIDTH as usize]> = (0..height)
+                .map(|_| std::array::from_fn(|_| rngs.gen_bool(0.5)))
+                .collect();
+
+            let marks = empty_spots
+                .iter()
+                // start from the top for the string
+                .rev()
+                .map(|row| {
+                    row.iter()
+                        .map(|&is_empty| if is_empty { EMPTY } else { EXISTS })
+                        .collect::<String>()
+                })
+                .collect::<Vec<String>>();
+
+            let field = field_factory::create_field_with_marks(marks.concat());
+
+            assert_eq!(
+                to_string_with_height(field.as_ref(), height),
+                marks.join("\n")
+            );
+        }
+    }
+
+    #[test]
+    fn test_random_with_height() {
+        let mut rngs = thread_rng();
+
+        for _ in 0..10000 {
+            let height = rngs.gen_range(1..=12);
+
+            // create field
+            let empty_spots: Vec<[_; FIELD_WIDTH as usize]> = (0..height)
+                .map(|_| std::array::from_fn(|_| rngs.gen_bool(0.5)))
+                .collect();
+
+            // parse to string for factory
+            let marks = empty_spots
+                .iter()
+                // start from the top for the string
+                .rev()
+                .map(|row| {
+                    row.iter()
+                        .map(|&is_empty| if is_empty { EMPTY } else { EXISTS })
+                        .collect::<String>()
+                })
+                .collect::<Vec<String>>();
+
+            let field = field_factory::create_field_with_marks(marks.concat());
+
+            let empty_line = (12 - height) % 6;
+            let expected = (0..empty_line)
+                .map(|_| "__________")
+                .chain(marks.iter().map(|string| string.as_str()))
+                .map(|str| str.to_string())
+                .collect::<Vec<String>>()
+                .join("\n");
+
+            assert_eq!(to_string_with_height(field.as_ref(), height), expected);
+        }
+    }
+}
