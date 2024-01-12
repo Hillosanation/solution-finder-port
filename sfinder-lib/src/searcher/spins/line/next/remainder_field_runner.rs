9e19778f0c285ecfffa3b5b3b82939cd85492446
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-12 22:00:29 +0800

    reduce logic handling different boards since only one row can have filled blocks, I can just use the logic from the branch using SmallField to handle the single row after it is shifted down

--- sfinder-lib/src/searcher/spins/line/next/remainder_field_runner.rs
+++ sfinder-lib/src/searcher/spins/line/next/remainder_field_runner.rs
@@ -2,8 +2,7 @@
 
 use super::remainder_field::RemainderField;
 use crate::sfinder_core::field::{
-    bit_operators, field::Field, field_constants::BoardCount, field_factory,
-    large_field::LargeField, middle_field::MiddleField, small_field::SmallField,
+    bit_operators, field::Field, field_factory, small_field::SmallField,
 };
 
 // TODO: there is an easier way of getting the row we want by ANDing the inverted init_field with the field with a filled row
@@ -14,20 +13,22 @@ pub fn extract(init_field: &dyn Field, target_y: u8) -> Vec<RemainderField> {
     let mut remainder_block = field_factory::create_field(max_field_height);
     remainder_block.fill_row(target_y);
     remainder_block.reduce(init_field);
+    remainder_block.slide_down(target_y);
 
-    extract_inner(remainder_block)
+    // only the lowermost row fill have filled blocks
+    let board = remainder_block.get_board(0);
+
+    extract_inner(Box::new(SmallField::from(board)))
 }
 
 fn extract_inner(mut remainder_block: Box<dyn Field>) -> Vec<RemainderField> {
-    let max_board_count = remainder_block.get_board_count();
-
     let mut pairs = Vec::new();
 
     assert!(!remainder_block.is_empty());
 
     while !remainder_block.is_empty() {
         let (remainder_field, ret_remainder_block) =
-            calc_remainder_field_pair(remainder_block.as_ref(), &max_board_count);
+            calc_remainder_field_pair(remainder_block.as_ref());
 
         pairs.push(remainder_field);
 
@@ -39,56 +40,15 @@ fn extract_inner(mut remainder_block: Box<dyn Field>) -> Vec<RemainderField> {
     pairs
 }
 
-fn calc_remainder_field_pair(
-    rest_block: &dyn Field,
-    max_board_count: &BoardCount,
-) -> (RemainderField, Box<dyn Field>) {
+fn calc_remainder_field_pair(rest_block: &dyn Field) -> (RemainderField, Box<dyn Field>) {
     debug_assert!(!rest_block.is_empty());
 
-    match max_board_count {
-        BoardCount::Small => {
-            let low = rest_block.get_board(0);
+    let low = rest_block.get_board(0);
 
-            {
-                let next_board = get_next_board(low).unwrap();
-                let next_rest_block = SmallField::from(next_board);
-                to_remainder_field_pair(low, next_board, Box::new(next_rest_block))
-            }
-        }
-        BoardCount::Middle => {
-            let low = rest_block.get_board(0);
-            let high = rest_block.get_board(1);
-
-            if let Some(next_board) = get_next_board(low) {
-                let next_rest_block = MiddleField::from_parts(next_board, high);
-                to_remainder_field_pair(low, next_board, Box::new(next_rest_block))
-            } else {
-                let next_board = get_next_board(high).unwrap();
-                let next_rest_block = MiddleField::from_parts(0, next_board);
-                to_remainder_field_pair(high, next_board, Box::new(next_rest_block))
-            }
-        }
-        BoardCount::Large => {
-            let low = rest_block.get_board(0);
-            let mid_low = rest_block.get_board(1);
-            let mid_high = rest_block.get_board(2);
-            let high = rest_block.get_board(3);
-
-            if let Some(next_board) = get_next_board(low) {
-                let next_rest_block = LargeField::from_parts(next_board, mid_low, mid_high, high);
-                to_remainder_field_pair(low, next_board, Box::new(next_rest_block))
-            } else if let Some(next_board) = get_next_board(mid_low) {
-                let next_rest_block = LargeField::from_parts(0, next_board, mid_high, high);
-                to_remainder_field_pair(mid_low, next_board, Box::new(next_rest_block))
-            } else if let Some(next_board) = get_next_board(mid_high) {
-                let next_rest_block = LargeField::from_parts(0, 0, next_board, high);
-                to_remainder_field_pair(mid_high, next_board, Box::new(next_rest_block))
-            } else {
-                let next_board = get_next_board(high).unwrap();
-                let next_rest_block = LargeField::from_parts(0, 0, 0, next_board);
-                to_remainder_field_pair(high, next_board, Box::new(next_rest_block))
-            }
-        }
+    {
+        let next_board = get_next_board(low).unwrap();
+        let next_rest_block = SmallField::from(next_board);
+        to_remainder_field_pair(low, next_board, Box::new(next_rest_block))
     }
 }
 
