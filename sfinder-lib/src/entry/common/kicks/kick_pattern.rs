e1eb62eb82630471c78c7261403506c0690afcbd
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-16 00:14:36 +0800

    implement replacement to KickPattern trait

--- sfinder-lib/src/entry/common/kicks/kick_pattern.rs
+++ sfinder-lib/src/entry/common/kicks/kick_pattern.rs
@@ -11,3 +11,49 @@ pub trait KickPattern {
         fallback: &'a BTreeMap<KickType, Box<dyn KickPattern>>,
     ) -> Option<&Pattern>;
 }
+
+pub enum KickPatternType {
+    Fixed { pattern: Pattern },
+    Referenced { reference_kick_type: KickType },
+}
+
+struct _KickPattern {
+    kick_type: KickType,
+    kick_pattern_type: KickPatternType,
+}
+
+impl _KickPattern {
+    pub fn new(kick_type: KickType, kick_pattern_type: KickPatternType) -> Self {
+        if let KickPatternType::Referenced {
+            reference_kick_type,
+        } = kick_pattern_type
+        {
+            assert_ne!(kick_type, reference_kick_type, "Cannot refer to itself");
+        }
+        Self {
+            kick_type,
+            kick_pattern_type,
+        }
+    }
+}
+
+impl KickPattern for _KickPattern {
+    fn get_kick_type(&self) -> &KickType {
+        &self.kick_type
+    }
+
+    fn get_pattern<'a>(
+        &'a self,
+
+        fallback: &'a BTreeMap<KickType, Box<dyn KickPattern>>,
+    ) -> Option<&Pattern> {
+        match &self.kick_pattern_type {
+            KickPatternType::Fixed { pattern } => Some(pattern),
+            KickPatternType::Referenced {
+                reference_kick_type,
+            } => fallback
+                .get(reference_kick_type)
+                .and_then(move |kick_pattern| kick_pattern.get_pattern(fallback)),
+        }
+    }
+}
