096e60934f6b0e95f49358edccfba5205917ed91
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-15 20:10:50 +0800

    remove OffsetDefine removes state, makes create_pattern easier to use since you don't have to initialize OffsetDefine

--- sfinder-lib/src/sfinder_core/srs/offset_define.rs
+++ sfinder-lib/src/sfinder_core/srs/offset_define.rs
@@ -13,56 +13,45 @@ pub enum OffsetType {
     Other,
 }
 
-pub struct OffsetDefine {
-    offset_type: OffsetType,
-    rotate_map: [Offset; Rotate::get_size()],
+#[rustfmt::skip]
+fn get_rotate_map(offset_type: OffsetType) -> [Offset; Rotate::get_size()] {
+    match offset_type {
+        OffsetType::I => [
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(-1, 0), Coordinate::new(2, 0), Coordinate::new(-1, 0), Coordinate::new(2, 0)]),
+            Offset::new(vec![Coordinate::new(-1, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 1), Coordinate::new(0, -2)]),
+            Offset::new(vec![Coordinate::new(-1, 1), Coordinate::new(1, 1), Coordinate::new(-2, 1), Coordinate::new(1, 0), Coordinate::new(-2, 0)]),
+            Offset::new(vec![Coordinate::new(0, 1), Coordinate::new(0, 1), Coordinate::new(0, 1), Coordinate::new(0, -1), Coordinate::new(0, 2)]),
+        ],
+        OffsetType::O => [
+            Offset::new(vec![Coordinate::new(0, 0)]),
+            Offset::new(vec![Coordinate::new(0, -1)]),
+            Offset::new(vec![Coordinate::new(-1, -1)]),
+            Offset::new(vec![Coordinate::new(-1, 0)]),
+        ],
+        OffsetType::T => [
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(1, 0), Coordinate::new(1, -1), Coordinate::new(0, 2), Coordinate::new(1, 2)]),
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(-1, 0), Coordinate::new(-1, -1), Coordinate::new(0, 2), Coordinate::new(-1, 2)]),
+        ],
+        OffsetType::Other => [
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(1, 0), Coordinate::new(1, -1), Coordinate::new(0, 2), Coordinate::new(1, 2)]),
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
+            Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(-1, 0), Coordinate::new(-1, -1), Coordinate::new(0, 2), Coordinate::new(-1, 2)]),
+        ],
+    }
 }
 
-impl OffsetDefine {
-    pub fn new(offset_type: OffsetType) -> Self {
-        #[rustfmt::skip]
-        let rotate_map = match offset_type {
-            OffsetType::I => [
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(-1, 0), Coordinate::new(2, 0), Coordinate::new(-1, 0), Coordinate::new(2, 0)]),
-                Offset::new(vec![Coordinate::new(-1, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 1), Coordinate::new(0, -2)]),
-                Offset::new(vec![Coordinate::new(-1, 1), Coordinate::new(1, 1), Coordinate::new(-2, 1), Coordinate::new(1, 0), Coordinate::new(-2, 0)]),
-                Offset::new(vec![Coordinate::new(0, 1), Coordinate::new(0, 1), Coordinate::new(0, 1), Coordinate::new(0, -1), Coordinate::new(0, 2)]),
-            ],
-            OffsetType::O => [
-                Offset::new(vec![Coordinate::new(0, 0)]),
-                Offset::new(vec![Coordinate::new(0, -1)]),
-                Offset::new(vec![Coordinate::new(-1, -1)]),
-                Offset::new(vec![Coordinate::new(-1, 0)]),
-            ],
-            OffsetType::T => [
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(1, 0), Coordinate::new(1, -1), Coordinate::new(0, 2), Coordinate::new(1, 2)]),
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(-1, 0), Coordinate::new(-1, -1), Coordinate::new(0, 2), Coordinate::new(-1, 2)]),
-            ],
-            OffsetType::Other => [
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(1, 0), Coordinate::new(1, -1), Coordinate::new(0, 2), Coordinate::new(1, 2)]),
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0), Coordinate::new(0, 0)]),
-                Offset::new(vec![Coordinate::new(0, 0), Coordinate::new(-1, 0), Coordinate::new(-1, -1), Coordinate::new(0, 2), Coordinate::new(-1, 2)]),
-            ],
-        };
+// Porting note: replaces getPattern
+pub fn create_pattern(offset_type: OffsetType, current: Rotate, next: Rotate) -> Pattern {
+    let rotate_map = get_rotate_map(offset_type);
 
-        Self {
-            offset_type,
-            rotate_map,
-        }
-    }
-
-    // Porting note: replaces getPattern
-    pub fn create_pattern(&self, current: Rotate, next: Rotate) -> Pattern {
-        // SRSでは、TSTフォームはRegular判定にする
-        if self.offset_type == OffsetType::T && matches!(next, Rotate::Right | Rotate::Left) {
-            // 「接着時にTが横向き and 回転テストパターンが最後(4)のケース」を格上げする
-            self.rotate_map[current as usize]
-                .to_pattern_with_privilege_spin(&self.rotate_map[next as usize], 4)
-        } else {
-            self.rotate_map[current as usize].to_pattern(&self.rotate_map[next as usize])
-        }
+    // SRSでは、TSTフォームはRegular判定にする
+    if offset_type == OffsetType::T && matches!(next, Rotate::Right | Rotate::Left) {
+        // 「接着時にTが横向き and 回転テストパターンが最後(4)のケース」を格上げする
+        rotate_map[current as usize].to_pattern_with_privilege_spin(&rotate_map[next as usize], 4)
+    } else {
+        rotate_map[current as usize].to_pattern(&rotate_map[next as usize])
     }
 }
