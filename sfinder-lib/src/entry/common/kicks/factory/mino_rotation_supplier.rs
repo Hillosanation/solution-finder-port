f16a84a2e24910472848272411e283968ad9a5b8
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-16 12:42:32 +0800

    remove MinoRotationSupplier struct functions moved to module

--- sfinder-lib/src/entry/common/kicks/factory/mino_rotation_supplier.rs
+++ sfinder-lib/src/entry/common/kicks/factory/mino_rotation_supplier.rs
@@ -15,79 +15,46 @@ type MapResult = Result<[Pattern; Piece::get_size() * Rotate::get_size()], KickT
 const SIZE_90_ONLY: usize = Piece::get_size() * Rotate::get_size() * 2;
 const SIZE_WITH_180: usize = Piece::get_size() * Rotate::get_size() * 3;
 
-pub struct MinoRotationSupplier {
-    kick_patterns: KickPatterns,
+pub fn create_new_rotation(kick_patterns: KickPatterns) -> Result<Box<dyn MinoRotation>, String> {
+    match kick_patterns.len() {
+        SIZE_90_ONLY => Ok(Box::new(MinoRotationNo180Impl::new(
+            create_map(&kick_patterns, RotateDirection::Clockwise).map_err(fmt_kick_type_error)?,
+            create_map(&kick_patterns, RotateDirection::CounterClockwise)
+                .map_err(fmt_kick_type_error)?,
+        ))),
+        SIZE_WITH_180 => Ok(Box::new(MinoRotationImpl::new(
+            create_map(&kick_patterns, RotateDirection::Clockwise).map_err(fmt_kick_type_error)?,
+            create_map(&kick_patterns, RotateDirection::CounterClockwise)
+                .map_err(fmt_kick_type_error)?,
+            create_map(&kick_patterns, RotateDirection::Rotate180).map_err(fmt_kick_type_error)?,
+        ))),
+        len => Err(format!("Invalid kick pattern size: {len}")),
+    }
 }
 
-impl MinoRotationSupplier {
-    pub fn new(kick_patterns: KickPatterns) -> Self {
-        Self::validate(&kick_patterns).unwrap();
-        Self { kick_patterns }
-    }
+fn fmt_kick_type_error(kick_type: KickType) -> String {
+    format!(
+        "Invalid kick type: piece={:?}, from={:?}, to={:?}",
+        kick_type.piece, kick_type.from, kick_type.to
+    )
+}
 
-    fn validate(kick_patterns: &KickPatterns) -> Result<(), String> {
-        let len = kick_patterns.len();
-        if !matches!(len, SIZE_90_ONLY | SIZE_WITH_180) {
-            return Err(format!("Unexpected number of kicks: size={len}"));
-        }
+fn create_map(kick_patterns: &KickPatterns, direction: RotateDirection) -> MapResult {
+    let result = (0..Piece::get_size() * Rotate::get_size())
+        .map(|i| {
+            let piece = Piece::new((i / Rotate::get_size()) as _);
+            let from = Rotate::new((i % Rotate::get_size()) as _);
 
-        let cw_map = Self::create_map(kick_patterns, RotateDirection::Clockwise)
-            .map_err(|kick| Self::fmt_kick_type_error(&kick))?;
+            let to = from.apply(direction);
+            kick_patterns
+                .get_pattern(piece, from, to)
+                .cloned()
+                .ok_or(KickType { piece, from, to })
+        })
+        .collect::<Result<Vec<_>, _>>()?
+        .try_into()
+        // never fails as size is determined by range
+        .unwrap();
 
-        let ccw_map = Self::create_map(kick_patterns, RotateDirection::CounterClockwise)
-            .map_err(|kick| Self::fmt_kick_type_error(&kick))?;
-
-        if Self::supports_180(len) {
-            let rotate_180_map = Self::create_map(kick_patterns, RotateDirection::Rotate180)
-                .map_err(|kick| Self::fmt_kick_type_error(&kick))?;
-        }
-
-        Ok(())
-    }
-
-    pub fn create_new_rotation(&self) -> Box<dyn MinoRotation> {
-        if Self::supports_180(self.kick_patterns.len()) {
-            Box::new(MinoRotationImpl::new(
-                Self::create_map(&self.kick_patterns, RotateDirection::Clockwise).unwrap(),
-                Self::create_map(&self.kick_patterns, RotateDirection::CounterClockwise).unwrap(),
-                Self::create_map(&self.kick_patterns, RotateDirection::Rotate180).unwrap(),
-            ))
-        } else {
-            Box::new(MinoRotationNo180Impl::new(
-                Self::create_map(&self.kick_patterns, RotateDirection::Clockwise).unwrap(),
-                Self::create_map(&self.kick_patterns, RotateDirection::CounterClockwise).unwrap(),
-            ))
-        }
-    }
-
-    fn fmt_kick_type_error(kick_type: &KickType) -> String {
-        format!(
-            "Invalid kick type: piece={:?}, from={:?}, to={:?}",
-            kick_type.piece, kick_type.from, kick_type.to
-        )
-    }
-
-    fn supports_180(len: usize) -> bool {
-        len == SIZE_WITH_180
-    }
-
-    fn create_map(kick_patterns: &KickPatterns, direction: RotateDirection) -> MapResult {
-        let result = (0..Piece::get_size() * Rotate::get_size())
-            .map(|i| {
-                let piece = Piece::new((i / Rotate::get_size()) as _);
-                let from = Rotate::new((i % Rotate::get_size()) as _);
-
-                let to = from.apply(direction);
-                kick_patterns
-                    .get_pattern(piece, from, to)
-                    .cloned()
-                    .ok_or(KickType { piece, from, to })
-            })
-            .collect::<Result<Vec<_>, _>>()?
-            .try_into()
-            // never fails as size is determined by range
-            .unwrap();
-
-        Ok(result)
-    }
+    Ok(result)
 }
