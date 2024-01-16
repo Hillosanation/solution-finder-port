57d9e7beb178f512e2632022305531fe21412799
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-17 00:05:13 +0800

    implement Reachable

--- sfinder-lib/src/common/datastore/action/mod.rs
+++ sfinder-lib/src/common/datastore/action/mod.rs
@@ -2,3 +2,4 @@ pub mod action;
 pub mod cache;
 pub mod candidate;
 pub mod minimal_action;
+pub mod reachable;
---
+++ sfinder-lib/src/common/datastore/action/reachable/reachable.rs
@@ -0,0 +1,8 @@
+use crate::sfinder_core::{field::field::Field, mino::mino::Mino};
+
+pub trait Reachable {
+    // checksを呼び出す前に、Field.cansPutの確認を必ずしていること
+    fn checks(field: &dyn Field, mino: &'static Mino, x: u8, y: u8, valid_height: u8) -> bool;
+}
+
+pub trait ILockedReachable: Reachable {}
