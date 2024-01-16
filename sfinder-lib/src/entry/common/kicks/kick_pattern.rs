fd280d0920d6a3b58a6e7f0abc9c867a38cf0ec1
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-16 10:57:06 +0800

    replace FixedKickPattern/ReferencedKickPattern

--- sfinder-lib/src/entry/common/kicks/kick_pattern.rs
+++ sfinder-lib/src/entry/common/kicks/kick_pattern.rs
@@ -2,33 +2,23 @@ use super::kick_type::KickType;
 use crate::sfinder_core::srs::pattern::Pattern;
 use std::collections::BTreeMap;
 
-pub trait KickPattern {
-    fn get_kick_type(&self) -> &KickType;
-
-    fn get_pattern<'a>(
-        &'a self,
-        // TODO: can we just use FixedKickPattern? I don't think you need multiple layers of indirection
-        fallback: &'a BTreeMap<KickType, Box<dyn KickPattern>>,
-    ) -> Option<&Pattern>;
-}
-
 pub enum KickPatternType {
     Fixed { pattern: Pattern },
     Referenced { reference_kick_type: KickType },
 }
 
-struct _KickPattern {
+pub struct KickPattern {
     kick_type: KickType,
     kick_pattern_type: KickPatternType,
 }
 
-impl _KickPattern {
+impl KickPattern {
     pub fn new(kick_type: KickType, kick_pattern_type: KickPatternType) -> Self {
         if let KickPatternType::Referenced {
-            reference_kick_type,
+            ref reference_kick_type,
         } = kick_pattern_type
         {
-            assert_ne!(kick_type, reference_kick_type, "Cannot refer to itself");
+            assert_ne!(kick_type, *reference_kick_type, "Cannot refer to itself");
         }
         Self {
             kick_type,
@@ -37,15 +27,15 @@ impl _KickPattern {
     }
 }
 
-impl KickPattern for _KickPattern {
-    fn get_kick_type(&self) -> &KickType {
+impl KickPattern {
+    pub fn get_kick_type(&self) -> &KickType {
         &self.kick_type
     }
 
-    fn get_pattern<'a>(
+    pub fn get_pattern<'a>(
         &'a self,
-
-        fallback: &'a BTreeMap<KickType, Box<dyn KickPattern>>,
+        // TODO: can we just use FixedKickPattern? I don't think you need multiple layers of indirection
+        fallback: &'a BTreeMap<KickType, KickPattern>,
     ) -> Option<&Pattern> {
         match &self.kick_pattern_type {
             KickPatternType::Fixed { pattern } => Some(pattern),
--- sfinder-lib/src/entry/common/kicks/mod.rs
+++ sfinder-lib/src/entry/common/kicks/mod.rs
@@ -1,5 +1,3 @@
 pub mod factory;
-pub mod fixed_kick_pattern;
 pub mod kick_pattern;
 pub mod kick_type;
-pub mod referenced_kick_pattern;
