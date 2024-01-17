6c25c8bd7687b7f834e87cc9ebb64a4ea0599285
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-17 17:04:50 +0800

    add coordinate_walker tests

--- sfinder-lib/src/sfinder_lib/coordinate_walker.rs
+++ sfinder-lib/src/sfinder_lib/coordinate_walker.rs
@@ -10,3 +10,83 @@ pub fn walk(mino: &'static Mino, max_y: u8) -> impl Iterator<Item = (u8, u8)> {
                 .map(move |x| (x, y))
         })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::sfinder_core::{
+        mino::{mino_factory::MinoFactory, piece::Piece},
+        srs::rotate::Rotate,
+    };
+    use std::collections::HashSet;
+
+    // Spawn, Right, Reverse, Left
+    fn test_wrapper(piece: Piece, sizes: [usize; Rotate::get_size()]) {
+        let mino_factory = MinoFactory::new();
+
+        for (&size, &rotate) in sizes.iter().zip(Rotate::value_list()) {
+            let mino = mino_factory.get(piece, rotate);
+            assert_eq!(walk(mino, 4).count(), size);
+        }
+    }
+
+    #[test]
+    fn walk_size_with_i() {
+        test_wrapper(Piece::I, [28, 10, 28, 10]);
+    }
+
+    #[test]
+    fn walk_size_with_j() {
+        test_wrapper(Piece::J, [24, 18, 24, 18]);
+    }
+
+    #[test]
+    fn walk_size_with_l() {
+        test_wrapper(Piece::L, [24, 18, 24, 18]);
+    }
+
+    #[test]
+    fn walk_size_with_o() {
+        test_wrapper(Piece::O, [27, 27, 27, 27]);
+    }
+
+    #[test]
+    fn walk_size_with_s() {
+        test_wrapper(Piece::S, [24, 18, 24, 18]);
+    }
+
+    #[test]
+    fn walk_size_with_z() {
+        test_wrapper(Piece::Z, [24, 18, 24, 18]);
+    }
+
+    #[test]
+    fn walk_size_with_t() {
+        test_wrapper(Piece::T, [24, 18, 24, 18]);
+    }
+
+    #[test]
+    fn walk_contains_with_i() {
+        let mino_factory = MinoFactory::new();
+        let coordinates =
+            walk(mino_factory.get(Piece::I, Rotate::Spawn), 4).collect::<HashSet<_>>();
+
+        for coord in &[(1, 0), (7, 0), (1, 3), (7, 3)] {
+            assert!(coordinates.contains(coord));
+        }
+
+        for coord in &[
+            (0, 0),
+            (8, 0),
+            (0, 3),
+            (8, 3),
+            // impossible by type
+            // (1, -1),
+            // (7, -1),
+            (1, 4),
+            (7, 4),
+        ] {
+            assert!(!coordinates.contains(coord));
+        }
+    }
+}
