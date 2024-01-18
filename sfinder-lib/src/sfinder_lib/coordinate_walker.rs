7aceebb7b1a74f681ba2e6446750090b9b97320d
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-18 14:54:17 +0800

    add get_ranges this should make iterating over a field with a mino easier by refactoring the casting clutter

--- sfinder-lib/src/sfinder_lib/coordinate_walker.rs
+++ sfinder-lib/src/sfinder_lib/coordinate_walker.rs
@@ -1,7 +1,10 @@
 use crate::sfinder_core::{field::field_constants::FIELD_WIDTH, mino::mino::Mino};
 
 // TODO: This does not use Coordinate because it uses i8
+#[cfg(test)]
 pub fn walk(mino: &'static Mino, max_y: u8) -> impl Iterator<Item = (u8, u8)> {
+    let (x_range, y_range) = get_ranges(mino, max_y);
+    let a = y_range.flat_map(move |y| x_range.clone().map(move |x| (x, y)));
     (u8::try_from(-mino.get_min_y()).unwrap()
         ..u8::try_from(max_y as i8 - mino.get_max_y()).unwrap())
         .flat_map(move |y| {
@@ -9,6 +12,18 @@ pub fn walk(mino: &'static Mino, max_y: u8) -> impl Iterator<Item = (u8, u8)> {
                 ..u8::try_from(FIELD_WIDTH as i8 - mino.get_max_x()).unwrap())
                 .map(move |x| (x, y))
         })
+}
+
+// (x_range, y_range)
+// TODO: refactor ranges of a mino over a field with this
+#[inline]
+pub fn get_ranges(mino: &'static Mino, max_y: u8) -> (std::ops::Range<u8>, std::ops::Range<u8>) {
+    (
+        u8::try_from(-mino.get_min_x()).unwrap()
+            ..u8::try_from(FIELD_WIDTH as i8 - mino.get_max_x()).unwrap(),
+        u8::try_from(-mino.get_min_y()).unwrap()
+            ..u8::try_from(max_y as i8 - mino.get_max_y()).unwrap(),
+    )
 }
 
 #[cfg(test)]
--- sfinder-lib/src/sfinder_lib/mod.rs
+++ sfinder-lib/src/sfinder_lib/mod.rs
@@ -1,6 +1,5 @@
 pub mod boolean_walker;
 // Porting note: replaces Coordinates
-#[cfg(test)]
 pub mod coordinate_walker;
 #[cfg(test)]
 pub mod randoms;
