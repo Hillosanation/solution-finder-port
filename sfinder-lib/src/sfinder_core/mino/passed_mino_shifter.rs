e1ff8ed791f69f6de13d5b24f6bf88996480f269
Hillosanation <88579655+Hillosanation@users.noreply.github.com>
2024-01-18 17:16:27 +0800

    add PassedMinoShifter tests

--- sfinder-lib/src/sfinder_core/mino/passed_mino_shifter.rs
+++ sfinder-lib/src/sfinder_core/mino/passed_mino_shifter.rs
@@ -38,3 +38,59 @@ impl PassedMinoShifter {
         Rotate::value_list().to_vec()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{sfinder_core::field::field_constants::FIELD_WIDTH, sfinder_lib::randoms};
+    use rand::{thread_rng, Rng};
+
+    #[test]
+    fn create_canonical_action() {
+        let mut rngs = thread_rng();
+        let mino_shifter = PassedMinoShifter::new();
+
+        for _ in 0..10000 {
+            let piece = randoms::gen_piece(&mut rngs);
+            let rotate = randoms::gen_rotate(&mut rngs);
+            let x = rngs.gen_range(0..FIELD_WIDTH);
+            let y = rngs.gen_range(0..12);
+            let action = MinimalAction::new(x, y, rotate);
+
+            let actual = mino_shifter.create_canonical_action(piece, rotate, x, y);
+
+            assert_eq!(actual, action);
+        }
+    }
+
+    #[test]
+    fn create_canonical_rotate() {
+        let mut rngs = thread_rng();
+        let mino_shifter = PassedMinoShifter::new();
+
+        for _ in 0..10000 {
+            let piece = randoms::gen_piece(&mut rngs);
+            let rotate = randoms::gen_rotate(&mut rngs);
+
+            let actual = mino_shifter.create_canonical_rotate(piece, rotate);
+
+            assert_eq!(actual, rotate);
+        }
+    }
+
+    #[test]
+    fn congruent_actions() {
+        let mut rngs = thread_rng();
+        let mino_shifter = PassedMinoShifter::new();
+
+        for _ in 0..10000 {
+            let piece = randoms::gen_piece(&mut rngs);
+            let rotate = randoms::gen_rotate(&mut rngs);
+            let x = rngs.gen_range(0..FIELD_WIDTH);
+            let y = rngs.gen_range(0..12);
+
+            let actual = mino_shifter.congruent_actions(piece, rotate, x, y);
+            assert_eq!(actual, vec![MinimalAction::new(x, y, rotate)]);
+        }
+    }
+}
